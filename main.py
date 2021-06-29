80affe745038005350c94e5cd50563d08a0a2bda
julienhirwa <juliushirwa@yahoo.fr>
2021-06-29 13:46:40 +0200

    finish settings file configurations

--- main.py
+++ main.py
@@ -1,13 +1,77 @@
+class DjangoDefault:
+  """
+    Set up Django Default files
+  """
+
+  def config_settings(self, project_name):
+    with open('{}/settings.py'.format(project_name), 'a') as settings:
+      settings.write('\n')
+      settings.write('# Django Graphene Boilerplate Configurations\n')
+      settings.write('import os\n\n')
+      settings.write('''
+
+# This apps should replace the Django Default ones on top 
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'graphene_django',
+]
+
+GRAPHENE = {
+    "SCHEMA": "%s.schema.schema",
+    "MIDDLEWARE": [
+        "graphql_jwt.middleware.JSONWebTokenMiddleware",
+        "graphene_django.debug.DjangoDebugMiddleware",
+        'graphene_django_extras.ExtraGraphQLDirectiveMiddleware'
+    ],
+    "SCHEMA_INDENT": 4,
+}
+
+# Add settings for authentication with graphql_jwt
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '%s/static/')
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+      ''' % (project_name, project_name))
+      settings.write('\n')
+
+
+  def config_schema(self, project_name):
+    with open('{}/schema.py'.format(project_name), 'a') as schema:
+      schema.write('\n')
+
+
 def main():
-  print('*****************************************************')
-  print('************ Django Graphene Boilerplate ************')
-  print('*****************************************************')
+  print('*****************************************************************************')
+  print('*****************************************************************************')
+  print('************************ Django Graphene Boilerplate ************************')
+  print('*****************************************************************************')
+  print('*****************************************************************************')
   import os
   import sys
   import subprocess
 
   args = sys.argv[1:]
   name_index = args.index("--name")
+  default = DjangoDefault()
 
 
   if len(args[name_index]):
@@ -21,21 +85,25 @@ def main():
 
       if venv.returncode == 0:
         print('Activating the virtual environment...')
-        os.system("source venv/bin/activate")
+        # os.system("")
         print('Installing dependencies...')
+        subprocess.call("source venv/bin/activate", shell=True, executable='/bin/bash')
         subprocess.run(["pip", "install", "django"])
         print('Initializing git repository...')
         subprocess.run(["git", "init"])
         print('Add virtual environment folder to ignored folders')
         subprocess.run(["touch", ".gitignore"])
-        # os.system('echo venv/\n >> .gitignore')
         print('Initializing Django default structure...')
         subprocess.run(["django-admin", "startproject", "{}".format(args[name_index+1]), "."])
         print('Migrating Django Default Migrations')
         os.system("python3 manage.py migrate")
-        # os.system("python3 manage.py runserver")
-        # os.system("rm -rf django_graphene_boilerplate venv .gitignore manage.py db.sqlite3")
+        print('Installing Graphene Django Extras...')
+        subprocess.run(["pip", "install", "graphene-django-extras"])
+        subprocess.run(["pip", "install", "django-graphql-jwt"])
+        subprocess.run(["pip", "install", "django-cors-headers"])
 
+        # configure .gitignore
+        print('Configuring .gitignore')
         with open('.gitignore', 'w') as gitignore:
             gitignore.write("venv/\n")
             gitignore.write("db.sqlite3\n")
@@ -44,8 +112,11 @@ def main():
 
         project_name = '{}'.format(args[name_index+1])
 
-        with open('{}/settings.py'.format(project_name), 'a') as settings:
-          print(settings)
+        print('Configuring Django Settings...')
+        default.config_settings(project_name)
+
+
+
 
 
 
